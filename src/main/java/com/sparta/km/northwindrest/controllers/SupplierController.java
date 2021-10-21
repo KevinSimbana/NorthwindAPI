46dfd0970c64a581295230d51dabcddf507f0c50
Kevin <48499023+KevinSimbana@users.noreply.github.com>
2021-10-21 22:23:16 +0100

    Initial setup for Supplier table.

---
+++ src/main/java/com/sparta/km/northwindrest/controllers/SupplierController.java
@@ -0,0 +1,24 @@
+package com.sparta.km.northwindrest.controllers;
+
+import com.sparta.km.northwindrest.entities.SupplierEntity;
+import com.sparta.km.northwindrest.repositories.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SupplierController {
+
+    private final SupplierRepository supplierRepository;
+
+    @Autowired
+    public SupplierController(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
+
+}
